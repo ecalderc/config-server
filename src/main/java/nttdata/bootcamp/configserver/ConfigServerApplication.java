834f439f26a02234ce59c91bea46c66730699fb8
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-25 15:20:02 -0500

    enable config server

--- src/main/java/nttdata/bootcamp/configserver/ConfigServerApplication.java
+++ src/main/java/nttdata/bootcamp/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package nttdata.bootcamp.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
